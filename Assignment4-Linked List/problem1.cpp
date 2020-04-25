62c9a8e91d97325269dc7d11faac38facc1f5101
dodooh <nguyen2697s@gmail.com>
2020-04-25 18:12:03 +0700

    Update problem1.cpp

--- Assignment4-Linked List/problem1.cpp
+++ Assignment4-Linked List/problem1.cpp
@@ -1,7 +1,126 @@
 #include <iostream>
-#include <NumberLinkedList.h>
-using namespace std;
 
+using namespace std;
+class NumberLinkedList {
+    private:
+    // Declare a structure for the list
+        struct ListNode
+        {
+        int value; // The value in this node
+        struct ListNode *next; // To point to the next node
+        };
+        ListNode *head; // List head pointer
+    public:
+        // Constructor
+        NumberLinkedList();
+        // Destructor
+        ~NumberLinkedList();
+        // Linked list operations
+        void insertFirst(int);
+        void insertLast(int);
+        void deleteFirst();
+        void deleteLast();
+        int length();
+        void deleteNode(int);
+        void displayList() const;
+};
+NumberLinkedList::NumberLinkedList()
+{   head = nullptr;}
+NumberLinkedList::~NumberLinkedList()
+{
+    ListNode* current = head;
+    ListNode* next;
+    while (current)
+    {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+}
+void NumberLinkedList::insertFirst(int item)
+{
+    ListNode *newNode = new ListNode;
+    newNode->value    = item;
+    newNode->next     = head;
+    head              = newNode;
+}
+void NumberLinkedList::insertLast(int item)
+{
+    ListNode *newNode = new ListNode;
+    newNode->value    = item;
+    newNode->next     = nullptr;
+    ListNode *current = head;
+    if (current == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+    while (current->next)
+        current = current->next;
+    current->next = newNode;
+}
+void NumberLinkedList::deleteFirst()
+{
+    if (head == nullptr)
+        return;
+    ListNode *temp = head;
+    head     = temp->next;
+}
+void NumberLinkedList::deleteLast()
+{
+    if (head == nullptr)
+        return;
+    if (head->next == nullptr)
+    {
+        free(head->next);
+        return;
+    }
+    ListNode *temp = head;
+    while (temp->next->next)
+        temp = temp->next;
+    temp->next = nullptr;
+}
+int NumberLinkedList::length()
+{
+    int c = 0;
+    ListNode *current = head;
+    while (current)
+    {
+        c++;
+        current = current->next;
+    }
+    return c;
+}
+void NumberLinkedList::deleteNode(int pos)
+{
+    if (pos == 0)
+    {
+        deleteFirst();
+        return;
+    }
+    ListNode *current = head;
+    for (int i = 0; i < pos-1 && current != nullptr ; ++i)
+    {
+        current = current->next;
+    }
+    if (current == nullptr || current->next == nullptr)
+    {
+        cout << "There is no node at position " << pos << endl;
+        return;
+    }
+    ListNode *posNext = current->next->next;
+    current->next = posNext;
+}
+void NumberLinkedList::displayList() const
+{
+    ListNode *current = head;
+    while (current)
+    {
+        cout << current->value << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
 int main()
 {
     NumberLinkedList ll;
